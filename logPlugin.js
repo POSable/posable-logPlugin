f6458b12bbff84620f9dd28f67a880bd821bd265
Nick Peters <nick@barbetsapp.com>
2015-11-13 14:49:42 -0700

    formatting cleanup

--- logPlugin.js
+++ logPlugin.js
@@ -10,32 +10,28 @@ LogPlugin.prototype.sendMsg = function(msg){
         this.msgLogger.addLogEntry(this.level, msg, stack); }
 
     if (typeof(msg) == 'object') {
-        this.msgLogger.addLogEntry(this.level, msg.message, msg.stack); }
-};
+        this.msgLogger.addLogEntry(this.level, msg.message, msg.stack); } };
 
 LogPlugin.prototype.fatal = function(msg) {
     if (this.fileLogger) {
         this.fileLogger.fatal(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.fatal) {
-        this.sendMsg(msg); }
-};
+        this.sendMsg(msg); } };
 
 LogPlugin.prototype.error = function(msg) {
     if (this.fileLogger) {
         this.fileLogger.error(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.error) {
-        this.sendMsg(msg); }
-};
+        this.sendMsg(msg); } };
 
 LogPlugin.prototype.info = function(msg) {
     if (this.fileLogger) {
         this.fileLogger.info(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.info) {
-        this.sendMsg(msg); }
-};
+        this.sendMsg(msg); } };
 
 LogPlugin.prototype.debug = function(msg) {
     if (this.fileLogger) {
@@ -43,17 +39,14 @@ LogPlugin.prototype.debug = function(msg) {
 
     if (this.msgLogger && this.level >= this.logLevels.debug) {
         console.log(msg);
-        this.sendMsg(msg); }
-};
+        this.sendMsg(msg); } };
 
 LogPlugin.prototype.setFileLogger = function(bunyanLogger) {
-    this.fileLogger = bunyanLogger;
-};
+    this.fileLogger = bunyanLogger; };
 
 LogPlugin.prototype.setMsgLogger = function(wascallyClient, level) {
     this.msgLogger = wascallyClient;
-    this.level = level;
-};
+    this.level = level; };
 
 LogPlugin.prototype.logLevels = { fatal: 60, error: 50, warn: 40, info: 30, debug: 20, trace: 10 };
 
