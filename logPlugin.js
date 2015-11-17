513ff5c4b45af1eb2b2f9e1dfa11a2cebc6f6843
Nick Peters <nick@barbetsapp.com>
2015-11-17 10:34:53 -0700

    add console log to debug / small refactor of fileLogger

--- logPlugin.js
+++ logPlugin.js
@@ -13,32 +13,32 @@ LogPlugin.prototype.sendMsg = function(msg){
         this.msgLogger.addLogEntry(this.level, msg.message, msg.stack); } };
 
 LogPlugin.prototype.fatal = function(msg) {
-    if (this.fileLogger && this.level >= this.fileLogger.level) {
+    if (this.fileLogger) {
         this.fileLogger.fatal(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.fatal) {
         this.sendMsg(msg); } };
 
 LogPlugin.prototype.error = function(msg) {
-    if (this.fileLogger && this.level >= this.fileLogger.level) {
+    if (this.fileLogger) {
         this.fileLogger.error(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.error) {
         this.sendMsg(msg); } };
 
 LogPlugin.prototype.info = function(msg) {
-    if (this.fileLogger && this.level >= this.fileLogger.level) {
+    if (this.fileLogger) {
         this.fileLogger.info(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.info) {
         this.sendMsg(msg); } };
 
 LogPlugin.prototype.debug = function(msg) {
-    if (this.fileLogger && this.level >= this.fileLogger.level) {
+    if (this.fileLogger) {
+        console.log(msg);
         this.fileLogger.debug(msg); }
 
     if (this.msgLogger && this.level >= this.logLevels.debug) {
-        console.log(msg);
         this.sendMsg(msg); } };
 
 LogPlugin.prototype.setFileLogger = function(bunyanLogger) {
